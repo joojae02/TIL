830fd42f3ec87391dacbdc08bf3431610165d3d6
wnwogus <jake010417@gmail.com>
2022-11-01 02:41:10 +0900

    2022-11-01

--- algorithm/floyd.cpp
+++ algorithm/floyd.cpp
@@ -8,21 +8,18 @@ void floyd2(int n, const int W[6][6], int D[6][6], int P[6][6]);
 void path(int q, int r);
 
 int main(void) {
-    int W[6][6] = {{0,0,0,0,0,0}, 
-						{0,0,1,INF,1,5},
-						{0,9,0,3,2,INF},
-						{0,INF,INF,0,4,INF},
-						{0,INF,INF,2,0,3},
-						{0,3,INF,INF,INF,0},
+    int W[6][6] = {     
+        {0,0,0,0,0,0}, 
+        {0,0,2,INF,1,INF},
+        {0,5,0,INF,3,INF},
+        {0,3,INF,0,2,INF},
+        {0,INF,INF,9,0,4},
+        {0,INF,3,INF,1,0},
         };
     int D[6][6];
     floyd2(5, W, D, P);
-    for(int k=1; k<= 5; k++){
-        for(int i=1; i <= 5; i++)     
-            cout << P[k][i] << " ";
-        cout << endl;
-    }
-    path(5,3);
+    
+    path(5,1);
 
 }
 
@@ -32,16 +29,48 @@ void floyd2(int n, const int W[6][6], int D[6][6], int P[6][6]){
     for(i=1; i <=n; i++)
         for(j=1; j <= n; j++)
             P[i][j] = 0;
+
     for(k=1; k<= n; k++)
         for(i=1; i <= n; i++)     
             D[k][i] = W[k][i];
-    for(k=1; k<=n; k++)
+    // cout << "0번째 D " << endl;
+    // for(int k=1; k<= 5; k++){
+    //     for(int i=1; i <= 5; i++){
+    //         if(D[k][i] == INF)
+    //             cout << "INF ";
+    //         else
+    //             cout << D[k][i] << " ";
+    //     }     
+    // cout << endl;
+    // }
+    cout << endl;
+    for(k=1; k<=n; k++){
         for(i=1; i<=n; i++)
             for(j=1; j<=n; j++)
             if (D[i][k] + D[k][j] < D[i][j]) {                
                 P[i][j] = k;
                 D[i][j] = D[i][k] + D[k][j];
-            }  
+            }
+        // cout << k << "번째 D " << endl;
+        // for(int k=1; k<= 5; k++){
+        //     for(int i=1; i <= 5; i++){
+        //         if(D[k][i] == INF)
+        //             cout << "INF ";
+        //         else
+        //             cout << D[k][i] << " ";
+        //     }     
+        //     cout << endl;
+        // }
+        // cout << endl;
+
+        // cout << k << "번째 P " << endl;
+        // for(int k=1; k<= 5; k++){
+        //     for(int i=1; i <= 5; i++)     
+        //         cout << P[k][i] << " ";
+        // cout << endl;
+        // }
+        // cout << endl;
+    }   
 }
 
 void path(int q, int r) {
